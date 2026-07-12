da74d450a1541ccceb65d798108e829ce1971837
Biraj Bhattarai <birajbhattarai299@gmail.com>
2026-07-12 13:38:48 +0545

    CRUD operations routes

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express();
 const path = require("path");
 const Listing = require("./models/listing.js");
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.set()
 const mongoose = require("mongoose");
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/BookingRaj';
@@ -17,13 +18,58 @@ async function main(){
 }
 
 
-
 app.get("/", (req,res)=>{
     res.send("Hi i am root");
 })
 
-app.get("/listings", (req,res)=>{
-    await 
+app.get("/listings", async (req,res)=>{
+    let allListings = await Listing.find({});
+    res.render("index.ejs",{allListings});
+})
+
+
+//post(add) new listings
+app.get("/listings/add", (req,res)=>{
+    res.render("addListing");
+})
+
+app.post("/listings", async (req,res)=>{
+    let {title,description,image,price,location,country} = req.body;
+    let newLisitng = new Listing({
+        title : title,
+        description : description,
+        image : image,
+        price : price,
+        location : location,
+        country : country,
+    });
+
+    await newLisitng.save();
+    res.send("data saved");
+})
+
+
+//update part
+app.get("/listings/:id/edit", async (req,res)=>{
+    let {id} = req.params;
+    console.log(id)
+    let listing = await Listing.find({_id : id});
+    console.log(listing);
+    res.send("worked")
+});
+
+app.patch("/listings/:id", (req,res)=>{
+    let {id} = req.params;
+    // Listing.findByIdAndUpdate("")
+
+});
+
+
+//delete part
+app.delete("/listings/:id",(req,res)=>{
+    let {id} = req.params;
+    Listing.findByIdAndDelete(id);
+    res.redirect("/");
 })
 
 
