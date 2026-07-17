3bf0c9e5b663cdb3373454c52bd277635d1b2ff0
Biraj Bhattarai <birajbhattarai299@gmail.com>
2026-07-17 21:25:14 +0545

    today final latest

--- models/listing.js
+++ models/listing.js
@@ -18,9 +18,9 @@ const Schema = {
             "https:/encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCBacBAmsH6NoiO0-otYwzP7D8dV1-LnMiqZDWXkZvBw&s=10" 
             : v
         },
-  price : string,
-  location : string,
-  country : string,
+  price : Number,
+  location : String,
+  country : String,
 }
 
 const ListingSchema = new mongoose.Schema(Schema);
