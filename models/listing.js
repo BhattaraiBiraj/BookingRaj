0593673bc5c74453ce0ec3b6d03cebfe727981fc
Biraj Bhattarai <birajbhattarai299@gmail.com>
2026-07-23 16:31:52 +0545

    reviews delete

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review.js");
 
 const Schema = {
     title : {
@@ -26,7 +27,15 @@ const Schema = {
              }],
 }
 
+
 const ListingSchema = new mongoose.Schema(Schema);
+
+ListingSchema.post("findOneAndDelete", async(listing)=>{
+    if(listing){
+        await Review.deleteMany({ _id: { $in: listing.reviews } })
+    }
+})
+
 const Listing = mongoose.model("Listing", ListingSchema);
 
 
