c97b2806313e706aee601b6f5fadc29047812017
Biraj Bhattarai <birajbhattarai299@gmail.com>
2026-07-26 16:52:24 +0545

    added new user model with passport-local-mongoose

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const schema = {
+    email : {
+        type : String,
+        required : true,
+    },
+};
+
+const userSchema = new mongoose.Schema(schema);
+userSchema.plugin(passportLocalMongoose);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
