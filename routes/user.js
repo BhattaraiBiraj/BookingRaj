ca356b1c961c9494343411b2a59d38117023459d
Biraj Bhattarai <birajbhattarai299@gmail.com>
2026-07-28 19:41:35 +0545

    login after signup

--- routes/user.js
+++ routes/user.js
@@ -12,10 +12,14 @@ router.post("/signup", WrapAsync(async (req,res)=>{
     try{
          let {username, email, password} = req.body;
         const user1 = new User({username, email});
-        const a = await User.register(user1, password);
-        console.log(a);
-        req.flash("success","Welcome to BookingRaj");
-        res.redirect("/listings");
+        const registeredUser = await User.register(user1, password);
+        req.login(registeredUser, (err)=>{
+            if(err){
+                return next(err);
+            }
+                req.flash("success","Welcome to BookingRaj");
+                res.redirect("/listings");  
+        })
     } catch(e){
         req.flash("error", e.message);
         res.redirect("/signup")
