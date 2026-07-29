b89025b6853659eaeb2bd9a87d3995c9959a48e9
Biraj Bhattarai <birajbhattarai299@gmail.com>
2026-07-29 20:59:17 +0545

    using router.route()

--- routes/listing.js
+++ routes/listing.js
@@ -20,25 +20,20 @@ const validateListing = (req,res,next) =>{
     }
 }
 
-//index
-router.get("/", wrapAsync(ListingController.index));
+router.route("/")
+.get( wrapAsync(ListingController.index))
+.post(isLoggedIn,validateListing,wrapAsync(ListingController.createListing))
 
 //post(add) new listings
 router.get("/new",isLoggedIn,ListingController.renderNewForm)
 
-router.post("/",isLoggedIn,validateListing,wrapAsync(ListingController.createListing))
-
-//show
-router.get("/:id",wrapAsync(ListingController.showListings));
-
 //update part
 router.get("/:id/edit", isLoggedIn,isOwner,wrapAsync(ListingController.renderEditForm));
 
-router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(ListingController.updateListing));
- 
-
-//delete part
-router.delete("/:id",isLoggedIn,wrapAsync(ListingController.destroyListing));
+router.route("/:id")
+.get("/:id",wrapAsync(ListingController.showListings))
+.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(ListingController.updateListing))
+.delete("/:id",isLoggedIn,wrapAsync(ListingController.destroyListing));
 
 
 module.exports = router;
